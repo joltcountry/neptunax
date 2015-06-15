1d160dcb79006c92ed191447f3061f6f9b619f7d
bparrish99 <ben@onecloudsys.com>
2015-06-15 17:06:20 -0500

    I have no idea what the christ is happening

---
+++ Neptunax/Assets/DoSomething.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoSomething : MonoBehaviour {
+
+	public void OnClick() {
+		print ("Robb sux");
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
